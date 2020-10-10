7989123a0d07ad43db3e9098474aca202a04b840
Donghoon Song <thdehdgns@gmail.com>
2020-10-11 00:08:33 +0900

    [add] getChallengeList

--- src/views/challenge/ChallengeList.vue
+++ src/views/challenge/ChallengeList.vue
@@ -3,8 +3,20 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
 	name: 'ChallengeList',
+	mounted() {
+		this.getChallengeList();
+	},
+	methods: {
+		getChallengeList() {
+			axios
+				.get('challengeList')
+				.then(res => console.log(res))
+				.catch(err => console.log(err));
+		},
+	},
 };
 </script>
 
