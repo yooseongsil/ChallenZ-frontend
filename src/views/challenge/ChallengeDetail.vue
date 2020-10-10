b1f8858a4f1fb035ce7669421eb9f9fb8aeda3d6
Donghoon Song <thdehdgns@gmail.com>
2020-10-11 00:34:51 +0900

    [add] getChallengeDetail

--- src/views/challenge/ChallengeDetail.vue
+++ src/views/challenge/ChallengeDetail.vue
@@ -3,8 +3,21 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
 	name: 'ChallengeDetail',
+	mounted() {
+		this.getChallengeDetail();
+	},
+	methods: {
+		getChallengeDetail() {
+			const id = '5f81be5fca8c69000932cdfd';
+			axios
+				.get(`challenge/${id}`)
+				.then(res => console.log(res))
+				.catch(err => console.log(err));
+		},
+	},
 };
 </script>
 
