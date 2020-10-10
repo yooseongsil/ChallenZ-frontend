59264f2da11a634b6609b1ff8d051bdbbfd2eea2
seongsil <yooo1201@naver.com>
2020-10-10 19:07:36 +0900

    [change] Typography

--- src/components/Typography.vue
+++ src/components/Typography.vue
@@ -25,21 +25,7 @@ export default {
 			default: null,
 			validator(value) {
 				// 값이 항상 아래 세 개의 값중 하나여야 합니다.
-				return (
-					[
-						'display1',
-						'headline1',
-						'headline2',
-						'headline3',
-						'headline4',
-						'headline5',
-						'headline6',
-						'body1',
-						'body2',
-						'caption1',
-						'caption2',
-					].indexOf(value) !== -1
-				);
+				return ['title', 'subTitle', 'body1', 'body2', 'caption'].indexOf(value) !== -1;
 			},
 		},
 	},
@@ -62,59 +48,29 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.display1 {
-	font-size: 42px;
-	line-height: 120%;
-	font-weight: bold;
-}
-.headline1 {
-	font-size: 32px;
-	line-height: 130%;
-	font-weight: bold;
-}
-.headline2 {
-	font-size: 28px;
-	line-height: 130%;
-	font-weight: bold;
-}
-.headline3 {
-	font-size: 26px;
-	line-height: 130%;
-	font-weight: bold;
-}
-.headline4 {
-	font-size: 24px;
-	line-height: 125%;
-	font-weight: bold;
-}
-.headline5 {
-	font-size: 22px;
-	line-height: 135%;
-	font-weight: normal;
-}
-.headline6 {
+.title {
 	font-size: 18px;
-	line-height: 135%;
-	font-weight: bold;
+	line-height: 22px;
+	font-weight: 600;
+}
+.subTitle {
+	font-size: 16px;
+	line-height: 20px;
+	font-weight: 600;
 }
 .body1 {
-	font-size: 16px;
-	line-height: 160%;
-	font-weight: normal;
+	font-size: 15px;
+	line-height: 150%;
+	font-weight: 600;
 }
 .body2 {
-	font-size: 14px;
-	line-height: 150%;
+	font-size: 13px;
+	line-height: 16px;
 	font-weight: normal;
 }
-.caption1 {
-	font-size: 12px;
-	line-height: 110%;
-	font-weight: 300;
-}
-.caption2 {
-	font-size: 10px;
-	line-height: 100%;
-	font-weight: 300;
+.caption {
+	font-size: 11px;
+	line-height: 13px;
+	font-weight: 500;
 }
 </style>
