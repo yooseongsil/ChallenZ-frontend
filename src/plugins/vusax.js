2b05675370601a52bacf93cf12494a29e81e9e03
seongsil <yooo1201@naver.com>
2020-10-10 21:49:24 +0900

    [change] colors

--- src/plugins/vusax.js
+++ src/plugins/vusax.js
@@ -1,13 +1,14 @@
 import Vue from 'vue';
 import Vuesax from 'vuesax';
+import { colors } from '@/components/color';
 
 import 'vuesax/dist/vuesax.css'; //Vuesax styles
 Vue.use(Vuesax, {
 	colors: {
-		primary: '#7E72F2',
-		success: 'rgb(23, 201, 100)',
-		danger: 'rgb(242, 19, 93)',
-		warning: 'rgb(255, 130, 0)',
-		dark: 'rgb(36, 33, 69)',
+		primary: colors['primary'],
+		success: colors['success'],
+		danger: colors['danger'],
+		warning: colors['warning'],
+		dark: colors['dark'],
 	},
 });
