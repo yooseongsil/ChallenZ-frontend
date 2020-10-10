b323e630401e5802506472385118f9c5e042bf4a
seongsil <yooo1201@naver.com>
2020-10-10 15:23:56 +0900

    [add] vuesax default color

--- src/plugins/vusax.js
+++ src/plugins/vusax.js
@@ -2,4 +2,12 @@ import Vue from 'vue';
 import Vuesax from 'vuesax';
 
 import 'vuesax/dist/vuesax.css'; //Vuesax styles
-Vue.use(Vuesax);
+Vue.use(Vuesax, {
+	colors: {
+		primary: '#7E72F2',
+		success: 'rgb(23, 201, 100)',
+		danger: 'rgb(242, 19, 93)',
+		warning: 'rgb(255, 130, 0)',
+		dark: 'rgb(36, 33, 69)',
+	},
+});
