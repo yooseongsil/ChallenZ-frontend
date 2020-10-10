408e73dfcd840f5f6575c6fc2d4651b94ee02a6b
seongsil <yooo1201@naver.com>
2020-10-10 16:34:47 +0900

    [add] scss

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -13,4 +13,31 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+	background-color: $white;
+	width: 100vw;
+	height: 48px;
+	box-shadow: 0 2px 10px rgba(0, 0, 0, 0.05);
+	position: fixed;
+	z-index: 10;
+	top: 0;
+	left: 0;
+
+	@include pc {
+		height: 70px;
+	}
+
+	.header_container {
+		height: 48px;
+		@include flexbox();
+		@include justify-content(space-between);
+		@include align-items(center);
+
+		@include pc {
+			height: 100%;
+			padding: 0;
+		}
+	}
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router';
 import store from './store';
 import './utils';
 import './plugins';
-import './assets/index.scss';
+import './assets/styles/base/index.scss';
 
 import instance from './utils/axios';
 Vue.prototype.$axios = instance;
