4cc7f972546df449341ba0c0dc3e5b1a61548ae2
hanaKim <hananumber1@nate.com>
2020-10-10 16:30:47 +0900

    [add] scss, elements,reset,index scss

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router';
 import store from './store';
 import './utils';
 import './plugins';
+import './assets/index.scss';
 
 import instance from './utils/axios';
 Vue.prototype.$axios = instance;
